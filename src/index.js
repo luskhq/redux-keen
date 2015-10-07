6a43db41c975fae50dfd12d2e2151fb5e139e9fa
Artur Müller <me@arturmuller.com>
2015-10-07 22:04:41 +0200

    Polish

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
-import Keen from 'keen-js';
-
-function isFunction(val) {
-  return typeof val === 'function';
-}
-
 export default function keenAnalytics(keenClient, getGlobals) {
-  if (!keenClient || !keenClient.addEvent) throw new Error('You must provide a keen-js client instance.');
+  if (!keenClient || !keenClient.addEvent) {
+    throw new TypeError('You must provide a keen-js client instance.');
+  }
 
   let globals = {};
 
-  return store => next => action => {
-    if (!action.meta ||
-      !action.meta.analytics ||
-      !action.meta.analytics.collection) {
+  return (store) => (next) => (action) => {
+    if (!action.meta || !action.meta.analytics || !action.meta.analytics.collection) {
       return next(action);
     }
 
     try {
-      if (isFunction(getGlobals)) {
+      if (typeof getGlobals === 'function') {
         globals = getGlobals(store.getState());
       }
 
       const { collection, event } = action.meta.analytics;
       keenClient.addEvent(collection, { ...globals, ...event });
-    } catch(error) {
+    }
+    catch (error) {
       console.error(error);
     }
     return next(action);
   };
-};
+}
